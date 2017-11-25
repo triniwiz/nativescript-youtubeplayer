9f342d9d2299602cbb3490c44451fdb30acd3adf
Osei Fortune <fortune.osei@yahoo.com>
2017-11-25 02:48:13 -0400

    Demo CI fix

--- demo/app/main-page.ts
+++ demo/app/main-page.ts
@@ -3,17 +3,17 @@ import * as pages from 'tns-core-modules/ui/page';
 import { HelloWorldModel } from './main-view-model';
 // Event handler for Page 'loaded' event attached in main-page.xml
 export function pageLoaded(args: observable.EventData) {
-    // Get the event sender
-    let page = <pages.Page>args.object;
-    page.bindingContext = new HelloWorldModel();
-    const player = page.getViewById('player');
-    player.on('playing', (args) => {
-        console.log('playing')
-    });
-    player.on('paused', (args) => {
-        console.log('paused')
-    });
-    player.on('fullScreen',(args)=>{
-        console.log(args.object.get('value'))
-    })
+  // Get the event sender
+  let page = <pages.Page>args.object;
+  page.bindingContext = new HelloWorldModel();
+  const player = page.getViewById('player');
+  player.on('playing', args => {
+    console.log('playing');
+  });
+  player.on('paused', args => {
+    console.log('paused');
+  });
+  player.on('fullScreen', args => {
+    console.log(args.object.get('value'));
+  });
 }
