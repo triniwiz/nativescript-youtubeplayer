1b55e971107a96f6d97d4bb43e2218548d42e999
Osei Fortune <fortune.osei@yahoo.com>
2017-11-28 14:30:41 -0400

    Load fix [Android]

--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -1,8 +1,17 @@
 import { Observable } from 'tns-core-modules/data/observable';
 export class HelloWorldModel extends Observable {
-  src = 'lwk5OUII9Vc';
+  src = 'L_jWHffIx5E';
   options = { rel: 1 };
   changeSource() {
+    this.set('src', 'dQw4w9WgXcQ');
+  }
+  getDanceHall() {
+    this.set('src', 'wH_0_pijbZY');
+  }
+  getHipHop() {
+    this.set('src', 'lwk5OUII9Vc');
+  }
+  getSoca() {
     this.set('src', 'vEG4qNW33mA');
   }
 }
